f4fbc365a82ca3e72d416aa8247dae9202c76f66
Constantinos Argyriou <rounnus@gmail.com>
2022-01-26 20:45:03 +0200

    Fix tool_builder.h

--- include/tool_builder.h
+++ include/tool_builder.h
@@ -16,71 +16,87 @@ struct command_d {
 	void *(*c_call_back)(const char *value);	// The action to take when the command has been requested.
 };
 
+struct builder_d {
+	struct command_d *commands;
+	// TODO - Add help struct.
+};
+
 
 /**
 	Builds a basic help action that prints informations
 	about the tool.
 	@param tool_name The name of the tool.
+	@param c_builder The builder to add the help.
 */
-extern void initialize_help(const char *tool_name);
+extern void initialize_help(struct builder_d *c_builder, const char *tool_name);
 
 /**
 	Adds user defined format of help.
 	If this function is used. Then the initialize_help
 	function must be ignored.
 	@param help The full help message.
+	@param c_builder The builder to add the help.
 */
-extern void add_full_help(const char *help);
+extern void add_full_help(struct builder_d *c_builder, const char *help);
 
 /**
 	Adds description to the help message.
 	@param description The description to be added.
+	@param c_builder The builder to add the description.
 */
-extern void add_help_tool_description(const char *description);
+extern void add_help_tool_description(struct builder_d *c_builder, const char *description);
 
 /**
 	Adds to the help action a command and the description
 	of the command.
 	@command_name The name of the command to add.
 	@command_description The description of the command.
+	@c_builder The builder to add the command and command description.
 */
-extern void add_help_tool_command(const char *command_name, const char *command_description);
+extern void add_help_tool_command(struct builder_d *c_builder, const char *command_name, 
+				  const char *command_description);
 
 /**
 	Adds a description in the end of the help message.
 	For example @copyright xxx or something like that.
 	@param description The description to be added.
+	@param c_builder The builder to add a closing descripton on help.
 */
-extern void add_help_tool_closing_description(const char *description);
+extern void add_help_tool_closing_description(struct builder_d *c_builder, const char *description);
 
 /**
 	Adds a new command.
+	@param c_builder The builder to add the command.
 	@param c_name The name of the command.
 	@param c_argc The arguments of the command.
 	@param c_alias The alias of the command.
 	@param c_call_back The callback that is executed then the command is requested.
 */
-extern void add_command(const char c_name[256], int c_argc,
-			const char c_alias[256][5], void *(*c_call_back)(const char *value));
+extern void add_command(struct builder_d *c_builder, const char c_name[256], 
+			int c_argc, const char c_alias[256][5], 
+			void *(*c_call_back)(const char *value));
 
 
 /**
 	Set or change the callback function of a specific
 	command.
+	@param c_builder The builder.
 	@param c_call_back The callback function to set or change with.
 	
 */
-extern void add_action(void *(*c_call_back)(const char *value));
+extern void add_action(struct builder_d *c_builder, void *(*c_call_back)(const char *value));
 
 
 /**
 	Adds a new command using by user defined value
 	using the struct.
+	@param c_builder The builder to add the command.
 	@param command The command to add.
 */
-static inline void add_command(const struct command_d *command) 
+static inline void add_command(struct builder_d *c_builder, const struct command_d *command) 
 {
 	add_command(
+		c_builder,
 		command->c_name,
 		command->c_argc,
 		command->c_alias,
@@ -93,8 +109,9 @@ static inline void add_command(const struct command_d *command)
 	Execute the action that has been requested.
 	@param argc The argc parameter of the main function.
 	@param argv The argv parameter of the main function.
+	@param c_builder The bulder who has the information for each command.
 */
-extern void execute_command(int argc, char **argv);
+extern void execute_command(int argc, char **argv, struct builder_d *c_builder);
 
 
 
