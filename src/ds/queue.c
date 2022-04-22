bdd973510c05bd3727073e0ef0e10993de7ff8df
Constantinos Argyriou <rounnus@gmail.com>
2022-04-22 14:46:01 +0300

    implement the interface of queue.

---
+++ src/ds/queue.c
@@ -0,0 +1,16 @@
+#include <queue.h>
+
+
+
+void tb_queue_enqueue(struct tb_queue *queue, struct tb_queue)
+{
+
+}
+
+
+void tb_queue_dequeue(struct tb_queue *queue)
+{
+
+}
+
+
---
+++ src/ds/queue.h
@@ -0,0 +1,51 @@
+#ifndef TOOL_BUILDER_QUEUE_H
+#define TOOL_BUILDER_QUEUE_H
+
+#include <tool_builder.h>
+#include <memory.h>
+
+// node data.
+struct tb_queue_node_d
+{
+	struct tool_builder_args c_args;
+	void (*c_callback)(const struct tool_builder_args *info);	
+};
+
+// queue node.
+struct tb_queue_node 
+{
+	// data
+	struct tb_queue_node_d data;
+	// link to next node.
+	struct tb_queue_node *next;
+};
+
+// queue.
+struct tb_queue 
+{
+	struct tb_queue_node *front; // the first element of queue.
+	struct tb_queue_node *rear;  // the last element of queue.
+};
+
+
+static inline void tb_queue_init(struct tb_queue *queue)
+{
+	memset(queue, 0x0, sizeof(struct tb_queue));
+}
+
+
+/**
+ *	Put's an element in queue.
+ *	@param queue The queue to put the element.
+*/
+extern void tb_queue_enqueue(struct tb_queue *queue, struct tb_queue);
+
+
+/**
+ *	Removes an element from queue.
+ *	@param queue The queue to remove the element.
+*/
+extern void tb_queue_dequeue(struct tb_queue *queue);
+
+
+#endif
